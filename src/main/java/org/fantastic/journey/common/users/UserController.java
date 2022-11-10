d62f41896f2b0767ac8aee282f35a99c6449c849
junhojava <junho9341@gmail.com>
2022-11-10 22:42:06 +0900

    add user controller

---
+++ src/main/java/org/fantastic/journey/common/users/UserController.java
@@ -0,0 +1,13 @@
+package org.fantastic.journey.common.users;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/user")
+    public String user() {
+        return "user is junho";
+    }
+}
