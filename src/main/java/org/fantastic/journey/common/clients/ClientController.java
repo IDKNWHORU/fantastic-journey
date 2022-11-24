928a641101782abda31282024305813c9a1bebd1
junhojava <junho9341@gmail.com>
2022-11-24 21:58:55 +0900

    update client datetype to char(8)

--- src/main/java/org/fantastic/journey/common/clients/ClientController.java
+++ src/main/java/org/fantastic/journey/common/clients/ClientController.java
@@ -20,34 +20,15 @@ public class ClientController {
 
     @PostMapping("/client")
     public Client createClient(@RequestBody Client newClient) {
-        Client client = Client.builder()
-                .name(newClient.getName())
-                .phoneNumber(newClient.getPhoneNumber())
-                .birthAt(newClient.getBirthAt())
-                .photoShot(newClient.getPhotoShot())
-                .memberId(newClient.getMemberId())
-                .cabinet(newClient.getCabinet())
-                .expiredAt(newClient.getExpiredAt())
-                .build();
-        clientRepository.insert(client);
+        clientRepository.insert(newClient);
 
-        return client;
+        return newClient;
     }
 
     @PutMapping("/client")
-    public Client updateClient(@RequestBody Client client) {
-        Client updatedClient = Client.builder()
-                .id(client.getId())
-                .name(client.getName())
-                .phoneNumber(client.getPhoneNumber())
-                .birthAt(client.getBirthAt())
-                .photoShot(client.getPhotoShot())
-                .memberId(client.getMemberId())
-                .cabinet(client.getCabinet())
-                .expiredAt(client.getExpiredAt())
-                .build();
+    public Client updateClient(@RequestBody Client updatedClient) {
         clientRepository.save(updatedClient);
 
-        return client;
+        return updatedClient;
     }
 }
